0050f92d318d9c4ea43e12fbd0046750119affc2
a.kurkin <a.kurkin@qiwi.com>
2023-09-18 21:07:49 +0300

    chat

---
+++ chat-client/src/main/java/org/example/Callback.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public interface Callback {
+    void call(Object... args);
+}
---
+++ chat-server/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args.length >= 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        Server server = new Server(port);
+        server.start();
+    }
+}
