ae41b2f55f2cd168a7abee2dc56df8c794dcf538
a.kurkin <a.kurkin@qiwi.com>
2023-09-18 22:24:40 +0300

    chat-start

--- chat-server/src/main/java/org/example/ClientHandler.java
+++ chat-server/src/main/java/org/example/ClientHandler.java
@@ -6,80 +6,88 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class ClientHandler {
-    private Socket socket;
+	private Socket socket;
 
-    private Server server;
-    private DataInputStream in;
-    private DataOutputStream out;
+	private Server server;
+	private DataInputStream in;
+	private DataOutputStream out;
 
-    private String username;
+	private String username;
 
-    private static int userCount = 0;
+	private static int userCount = 0;
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public ClientHandler(Socket socket, Server server) throws IOException {
-        this.socket = socket;
-        this.server = server;
-        in = new DataInputStream(socket.getInputStream());
-        out = new DataOutputStream(socket.getOutputStream());
-        username = "User" + userCount++;
-        server.subscribe(this);
-        new Thread(() -> {
-                try {
-                    while(true) {
-                        // /exit -> disconnect()
-                        // /w user message -> user
+	public ClientHandler(Socket socket, Server server) throws IOException {
+		this.socket = socket;
+		this.server = server;
+		in = new DataInputStream(socket.getInputStream());
+		out = new DataOutputStream(socket.getOutputStream());
+		username = "User" + userCount++;
+		server.subscribe(this);
+		new Thread(() -> {
+			try {
+				while (true) {
+					// /exit -> disconnect()
+					// /w user message -> user
 
-                        String message = in.readUTF();
-                        if(message.startsWith("/")) {
-                            if(message.equals("/exit")) {
-                                break;
-                            }
-                        }
-                        server.broadcastMessage("Server: " + message);
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } finally {
-                    disconnect();
-                }
-            }).start();
-    }
+					String message = in.readUTF();
+					if (message.startsWith("/")) {
+						String[] args = message.split(" ");
+						String command = args[0];
+						if (command.equals("/exit")) {
+							disconnect();
+							break;
+						}
+						if (command.equals("/w")) {
+							String username = args[1];
+							server.sendMessageToUser(username, message.substring(4 + username.length()));
+						}
+					} else {
+						server.broadcastMessage("Server: " + message);
+					}
+				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			} finally {
+				disconnect();
+			}
+		}).start();
+	}
 
-    public void disconnect() {
-        server.unsubscribe(this);
-        if(socket != null) {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        if (in != null) {
-            try {
-                in.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        if (out != null) {
-            try {
-                out.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
+	public void disconnect() {
+		server.unsubscribe(this);
+		if (socket != null) {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		if (in != null) {
+			try {
+				in.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		if (out != null) {
+			try {
+				out.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
 
-    public void sendMessage(String message) {
-        try {
-            out.writeUTF(message);
-        } catch (IOException e) {
-            e.printStackTrace();
-            disconnect();
-        }
-    }
+	public void sendMessage(String message) {
+		try {
+			out.writeUTF(message + "\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+			disconnect();
+		}
+	}
 }
